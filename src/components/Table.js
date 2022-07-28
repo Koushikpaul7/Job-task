df546bc6b53afde8c1426789f515f9d2ab5bb01e
Koushikpaul7 <koushikpaul6211@gmail.com>
2022-07-29 01:57:05 +0600

    final site

---
+++ src/components/Table.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+function Table({tableData}){
+    return(
+        <table className="table">
+            <thead>
+                <tr>
+                    <th>SL No.</th>
+                    <th>Date </th>
+                    <th>Amount</th>
+                    <th>Payment mode</th>
+                    <th>Remark</th>
+                </tr>
+            </thead>
+            <tbody>
+            {
+                tableData.map((data, index)=>{
+                    return(
+                        <tr key={index}>
+                            <td>{index+1}</td>
+                            <td>{data.date}</td>
+                            <td>{data.amount}</td>
+                            <td>{data.paymentMode}</td>
+                            <td>{data.remark}</td>
+                        </tr>
+                    )
+                })
+            }
+            </tbody>
+        </table>
+    )
+}
+export default Table;
+
